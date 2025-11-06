c001600a5297a8dfdbd7a648c28450f40a7696e8
Francesco Marino <franz984.marino@gmail.com>
2025-11-06 11:13:47 +0000

    Gemini rendimi lo sfondo scuro nell'app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="it" suppressHydrationWarning>
+    <html lang="it" suppressHydrationWarning className="dark">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
