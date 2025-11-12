d0b71d18ce688b4e2dc2fc34c2bbef8f81547694
Francesco Marino <franz984.marino@gmail.com>
2025-11-12 14:15:55 +0000

    Try fixing this error: `Runtime Error: Error: Megaphone is not defined.

--- src/app/dashboard/admin-dashboard.tsx
+++ src/app/dashboard/admin-dashboard.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Users, CalendarCheck, Warehouse, Package, ClipboardCheck } from 'lucide-react';
+import { Users, CalendarCheck, Warehouse, Package, ClipboardCheck, Megaphone } from 'lucide-react';
 import Link from 'next/link';
 import { Badge } from '@/components/ui/badge';
 
