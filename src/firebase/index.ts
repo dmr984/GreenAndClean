4c64d0b6abc66f56bca1fe7f10e334a8da88de49
Francesco Marino <franz984.marino@gmail.com>
2025-11-13 10:19:20 +0000

    Try fixing this error: `Runtime Error: Error: Module [project]/node_modu

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -35,6 +35,5 @@ export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
 export * from './non-blocking-updates';
-// non-blocking-login is removed as we use a custom auth system now
 export * from './errors';
 export * from './error-emitter';
